8094875003131647c84bf3fa9415c2e9fd05419c
Smit Patel <smitpatel316@gmail.com>
2020-06-08 11:34:37 -0400

    init

---
+++ A1/autograder.py
@@ -0,0 +1,336 @@
+import pickle
+
+from rushhour import *
+# import student's functions
+from solution import *
+
+
+def main():
+    # Select what to test
+    test_min_moves = True
+    test_fval_function = False
+    test_alternate = True
+    test_anytime_gbfs = False
+    test_anytime_weighted_astar = False
+
+    # load the test problems
+    TESTPROBLEMS = []
+    with (open("rushhour_tests.pkl", "rb")) as openfile:
+        while True:
+            try:
+                TESTPROBLEMS.append(pickle.load(openfile))
+            except EOFError:
+                break
+
+    if test_min_moves:
+
+        ##############################################################
+        # TEST MIN MOVES HEURISTIC
+        print("TESTING MIN MOVES")
+        PROBLEMS = TESTPROBLEMS[40:]
+
+        # Correct MIN MOVES distances for the initial states of the provided problem set
+        correct_min_moves = [
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            3,
+            7,
+            10,
+            14,
+            14,
+            17,
+            24,
+            28,
+            31,
+            29,
+        ]
+
+        solved = 0
+        unsolved = []
+
+        for i in range(0, len(PROBLEMS)):
+            print("PROBLEM {}".format(i))
+
+            s0 = PROBLEMS[i]
+
+            min_moves = heur_min_moves(s0)
+            print("calculated min_moves:", str(min_moves))
+            # To see state uncomment below
+            # print(s0.state_string())
+
+            if min_moves == correct_min_moves[i]:
+                solved += 1
+            else:
+                unsolved.append(i)
+
+        print("*************************************")
+        print(
+            "In the problem set provided, you calculated the correct Min Moves distance for {} states out of {}.".format(
+                solved, len(PROBLEMS)
+            )
+        )
+        print("States that were incorrect: {}".format(unsolved))
+        print("*************************************\n")
+        ##############################################################
+
+    if test_alternate:
+
+        ##############################################################
+        # TEST ALTERNATE HEURISTIC
+        print("Testing alternate heuristic with best_first search")
+
+        benchmark = 15
+        solved = 0
+        unsolved = []
+        timebound = 1  # time limit
+
+        PROBLEMS = TESTPROBLEMS[20:40]
+
+        for i in range(0, len(PROBLEMS)):
+
+            print("*************************************")
+            print("PROBLEM {}".format(i))
+
+            s0 = PROBLEMS[i]  # Problems get harder as i gets bigger
+            se = SearchEngine("best_first", "full")
+            se.init_search(s0, goal_fn=rushhour_goal_fn, heur_fn=heur_alternate)
+            final = se.search(timebound)
+
+            if final:
+                solved += 1
+            else:
+                unsolved.append(i)
+
+        print("\n*************************************")
+        print(
+            "Of {} initial problems, {} were solved in less than {} seconds by this solver.".format(
+                len(PROBLEMS), solved, timebound
+            )
+        )
+        print(
+            "Problems that remain unsolved in the set are Problems: {}".format(unsolved)
+        )
+        print(
+            "Heur min moves alone solved {} out of {} practice problems given {} seconds.".format(
+                benchmark, len(PROBLEMS), timebound
+            )
+        )
+        print("*************************************\n")
+        ##############################################################
+
+    if test_fval_function:
+
+        test_state = Rushhour("START", 6, None, None, None)
+
+        correct_fvals = [6, 11, 16, 1006]
+
+        ##############################################################
+        # TEST fval_function
+        print("*************************************")
+        print("Testing fval_function")
+
+        solved = 0
+        weights = [0.0, 0.5, 1.0, 100]
+        for i in range(len(weights)):
+
+            test_node = sNode(test_state, hval=10, fval_function=fval_function)
+
+            fval = fval_function(test_node, weights[i])
+            print(
+                "Test",
+                str(i),
+                "calculated fval:",
+                str(fval),
+                "correct:",
+                str(correct_fvals[i]),
+            )
+
+            if fval == correct_fvals[i]:
+                solved += 1
+
+        print("\n*************************************")
+        print(
+            "Your fval_function calculated the correct fval for {} out of {} tests.".format(
+                solved, len(correct_fvals)
+            )
+        )
+        print("*************************************\n")
+        ##############################################################
+
+    if test_anytime_gbfs:
+
+        ##############################################################
+        # TEST ANYTIME GBFS
+        print("Testing Anytime GBFS")
+
+        improved = 0
+        same = 0
+        worsened = 0
+        solved = 0
+        unsolved = []
+        timebound1 = 1
+        timebound2 = 5
+        # 1 second vs 5 seconds
+        PROBLEMS = TESTPROBLEMS[20:40]  # first ones are too easy!
+
+        for i in range(0, len(PROBLEMS)):
+
+            print("*************************************")
+            print("PROBLEM {}".format(i))
+
+            s0 = PROBLEMS[i]  # Problems get harder as i gets bigger
+            final1 = anytime_gbfs(s0, heur_fn=heur_min_moves, timebound=timebound1)
+            final2 = anytime_gbfs(s0, heur_fn=heur_min_moves, timebound=timebound2)
+
+            if not final2 and not final1:
+                print(
+                    "Problem {}: not solveable with more time (no solution vs. no solution).".format(
+                        i
+                    )
+                )
+                unsolved.append(i)
+            elif final1 and not final2:
+                worsened += 1
+                print(
+                    "Problem {}: ERROR -- not solveable with more time ({} vs. no solution).".format(
+                        i, final1.gval
+                    )
+                )
+            elif final2 and not final1:
+                solved += 1
+                print(
+                    "Problem {}: solveable with more time (no solution vs. {}).".format(
+                        i, final2.gval
+                    )
+                )
+            elif final2.gval == final1.gval:
+                same += 1
+                print(
+                    "Problem {}: same with more time (length {} vs. {}).".format(
+                        i, final1.gval, final2.gval
+                    )
+                )
+            elif final2.gval < final1.gval:
+                improved += 1
+                print(
+                    "Problem {}: improved with more time (length {} vs. {}).".format(
+                        i, final1.gval, final2.gval
+                    )
+                )
+            else:
+                worsened += 1
+                print(
+                    "Problem {}: ERROR -- longer solution with more time (length {} vs. {}).".format(
+                        i, final1.gval, final2.gval
+                    )
+                )
+
+        print("\n*************************************")
+        print(
+            "More time allows {} unsolved problems to be solved, improved solution lengths of {} problems, produced equal length solutions for {} problems.".format(
+                solved, improved, same
+            )
+        )
+        print("More time worsened {} solution lengths".format(worsened))
+        print(
+            "Problems that remain unsolved in the set are Problems: {}".format(unsolved)
+        )
+        print("*************************************\n")
+
+    if test_anytime_weighted_astar:
+
+        ##############################################################
+        # TEST ANYTIME WEIGHTED A STAR
+        print("Testing Anytime Weighted A Star")
+
+        improved = 0
+        same = 0
+        worsened = 0
+        solved = 0
+        unsolved = []
+        timebound1 = 1
+        timebound2 = 5
+        # 1 second vs 5 seconds
+        PROBLEMS = TESTPROBLEMS[20:40]  # first ones are too easy!
+
+        for i in range(0, len(PROBLEMS)):
+            print("*************************************")
+            print("PROBLEM {}".format(i))
+
+            s0 = PROBLEMS[i]  # Problems get harder as i gets bigger
+            weight = 100
+            final1 = anytime_weighted_astar(
+                s0, heur_fn=heur_min_moves, weight=weight, timebound=timebound1
+            )
+            final2 = anytime_weighted_astar(
+                s0, heur_fn=heur_min_moves, weight=weight, timebound=timebound2
+            )
+
+            if not final2 and not final1:
+                print(
+                    "Problem {}: not solveable with more time (no solution vs. no solution).".format(
+                        i
+                    )
+                )
+                unsolved.append(i)
+            elif final1 and not final2:
+                worsened += 1
+                print(
+                    "Problem {}: ERROR -- not solveable with more time ({} vs. no solution).".format(
+                        i, final1.gval
+                    )
+                )
+            elif final2 and not final1:
+                solved += 1
+                print(
+                    "Problem {}: solveable with more time (no solution vs. {}).".format(
+                        i, final2.gval
+                    )
+                )
+            elif final2.gval == final1.gval:
+                same += 1
+                print(
+                    "Problem {}: same with more time (length {} vs. {}).".format(
+                        i, final1.gval, final2.gval
+                    )
+                )
+            elif final2.gval < final1.gval:
+                improved += 1
+                print(
+                    "Problem {}: improved with more time (length {} vs. {}).".format(
+                        i, final1.gval, final2.gval
+                    )
+                )
+            else:
+                worsened += 1
+                print(
+                    "Problem {}: ERROR -- longer solution with more time (length {} vs. {}).".format(
+                        i, final1.gval, final2.gval
+                    )
+                )
+
+        print("\n*************************************")
+        print(
+            "More time allows {} unsolved problems to be solved, improved solution lengths of {} problems, produced equal length solutions for {} problems.".format(
+                solved, improved, same
+            )
+        )
+        print("More time worsened {} solution lengths".format(worsened))
+        print(
+            "Problems that remain unsolved in the set are Problems: {}".format(unsolved)
+        )
+        print("*************************************\n")
+
+        ##############################################################
+
+
+if __name__ == "__main__":
+    main()
---
+++ A1/solution.py
@@ -0,0 +1,313 @@
+#   Look for #IMPLEMENT tags in this file. These tags indicate what has
+#   to be implemented to complete the warehouse domain.
+
+#   You may add only standard python imports---i.e., ones that are automatically
+#   available on TEACH.CS
+#   You may not remove any imports.
+#   You may not import or otherwise source any of your own files
+
+
+# RUSH HOUR GOAL TEST
+import os
+
+from rushhour import Rushhour, Vehicle
+from search import SearchEngine
+
+
+class BoardHelper:
+    def __init__(self, state: Rushhour):
+        self.state = state
+
+        goal = state.get_board_properties()
+        board_size: (int, int) = goal[0]
+        self.total_x = board_size[1]
+        self.total_y = board_size[0]
+
+        self.goal_entrance: (int, int) = goal[1]
+        self.goal_direction: str = goal[2]
+        self.is_entrance_inverted = self.goal_direction in ["E", "S"]
+
+        for vehicle in self.state.vehicle_list:
+            if vehicle.is_goal:
+                self.goal_vehicle: Vehicle = vehicle
+
+        self.goal_vehicle_tail = self.get_vehicle_tail(self.goal_vehicle)
+
+    def get_vehicle_tail(self, vehicle: Vehicle) -> (int, int):
+        head = vehicle.loc
+        if vehicle.is_horizontal:
+            new_x = head[0] + vehicle.length - 1
+            if new_x >= self.total_x:
+                new_x -= self.total_x
+            return (new_x, head[1])
+        else:
+            new_y = head[1] + vehicle.length - 1
+            if new_y >= self.total_y:
+                new_y -= self.total_y
+            return (head[0], new_y)
+
+
+def rushhour_goal_fn(state: Rushhour):
+    """Have we reached a goal state?"""
+    helper = BoardHelper(state)
+
+    if helper.goal_vehicle is None:
+        return None
+
+    if helper.is_entrance_inverted:
+        return helper.goal_vehicle_tail == helper.goal_entrance
+    else:
+        return helper.goal_vehicle.loc == helper.goal_entrance
+
+
+# RUSH HOUR HEURISTICS
+def heur_zero(state):
+    # IMPLEMENT
+    """Zero Heuristic can be used to make A* search perform uniform cost search"""
+    return 0  # replace this
+
+
+def heur_min_moves_wrapper(state, helper=None):
+    if helper is None:
+        helper = BoardHelper(state)
+    if helper.goal_vehicle.is_horizontal:
+        goal_vehicle_x = helper.goal_vehicle.loc[0]
+        entrance_x = helper.goal_entrance[0]
+        # E
+        if helper.is_entrance_inverted:
+            goal_vehicle_x = helper.goal_vehicle_tail[0]
+
+        # Entrance is behind
+        if entrance_x > goal_vehicle_x:
+            moves_1 = goal_vehicle_x + helper.total_x - entrance_x
+            moves_2 = entrance_x - goal_vehicle_x
+        else:
+            moves_1 = goal_vehicle_x - entrance_x
+            moves_2 = helper.total_x - goal_vehicle_x + entrance_x
+    else:
+        goal_vehicle_y = helper.goal_vehicle.loc[1]
+        entrance_y = helper.goal_entrance[1]
+        # S
+        if helper.is_entrance_inverted:
+            goal_vehicle_y = helper.goal_vehicle_tail[1]
+
+        # Entrance is below
+        if entrance_y > goal_vehicle_y:
+            moves_1 = goal_vehicle_y + helper.total_y - entrance_y
+            moves_2 = entrance_y - goal_vehicle_y
+        else:
+            moves_1 = goal_vehicle_y - entrance_y
+            moves_2 = helper.total_y - goal_vehicle_y + entrance_y
+    return moves_1, moves_2
+
+
+def heur_min_moves(state):
+    # IMPLEMENT
+    """basic rushhour heuristic"""
+    # An admissible heuristic is nice to have. Getting to the goal may require
+    # many moves and each moves the goal vehicle one tile of distance.
+    # Since the board wraps around, there are two different
+    # directions that lead to the goal.
+    # NOTE that we want an estimate of the number of ADDITIONAL
+    #     moves required from our current state
+    # 1. Proceeding in the first direction, let MOVES1 =
+    #   number of moves required to get to the goal if it were unobstructed
+    # 2. Proceeding in the second direction, let MOVES2 =
+    #   number of moves required to get to the goal if it were unobstructed
+    #
+    # Our heuristic value is the minimum of MOVES1 and MOVES2 over all goal vehicles.
+    # You should implement this heuristic function exactly, even if it is
+    # tempting to improve it.
+
+    return min(*heur_min_moves_wrapper(state))
+
+
+def heur_alternate(state: Rushhour):
+    # IMPLEMENT
+    """a better heuristic"""
+    """INPUT: a rush hour state"""
+    """OUTPUT: a numeric value that serves as an estimate of the distance of the state to the goal."""
+    # heur_min_moves has an obvious flaw.
+    # Write a heuristic function that improves a little upon heur_min_moves to estimate distance between the current state and the goal.
+    # Your function should return a numeric value for the estimate of the distance to the goal.
+    helper = BoardHelper(state)
+    moves_1, moves_2 = heur_min_moves_wrapper(state, helper)
+
+    blocked_1 = 1
+    if helper.goal_vehicle.is_horizontal:
+        goal_vehicle_x = helper.goal_vehicle.loc[0]
+        entrance_x = helper.goal_entrance[0]
+        # E
+        if helper.is_entrance_inverted:
+            # goal_vehicle_x = helper.goal_vehicle_tail[0]
+            entrance_x -= helper.goal_vehicle.length - 1
+            if entrance_x < 0:
+                entrance_x = helper.total_x - entrance_x
+
+        for vehicle in state.vehicle_list:
+            if vehicle.loc != helper.goal_vehicle.loc and not vehicle.is_horizontal:
+                vehicle_tail = helper.get_vehicle_tail(vehicle)
+                if vehicle.loc[1] <= helper.goal_vehicle.loc[1] <= vehicle_tail[1]:
+                    if entrance_x > goal_vehicle_x:
+                        if vehicle.loc[0] < goal_vehicle_x or vehicle.loc[0] > entrance_x:
+                            blocked_1 += 1
+                    else:
+                        if goal_vehicle_x > vehicle.loc[0] > entrance_x:
+                            blocked_1 += 1
+    else:
+        goal_vehicle_y = helper.goal_vehicle.loc[1]
+        entrance_y = helper.goal_entrance[1]
+        # E
+        if helper.is_entrance_inverted:
+            # goal_vehicle_y = helper.goal_vehicle_tail[1]
+            entrance_y -= helper.goal_vehicle.length - 1
+            if entrance_y < 0:
+                entrance_y = helper.total_y - entrance_y
+
+        for vehicle in state.vehicle_list:
+            if vehicle.loc != helper.goal_vehicle.loc and vehicle.is_horizontal:
+                vehicle_tail = helper.get_vehicle_tail(vehicle)
+                if vehicle.loc[0] <= helper.goal_vehicle.loc[0] <= vehicle_tail[0]:
+                    if entrance_y > goal_vehicle_y:
+                        if vehicle.loc[1] < goal_vehicle_y or vehicle.loc[1] > entrance_y:
+                            blocked_1 += 1
+                    else:
+                        if goal_vehicle_y > vehicle.loc[1] > entrance_y:
+                            blocked_1 += 1
+    blocked_2 = 1
+    if helper.goal_vehicle.is_horizontal:
+        goal_vehicle_x = helper.goal_vehicle_tail[0]
+        entrance_x = helper.goal_entrance[0]
+        # E
+        if not helper.is_entrance_inverted:
+            # goal_vehicle_x = helper.goal_vehicle_tail[0]
+            entrance_x += helper.goal_vehicle.length - 1
+            if entrance_x >= helper.total_x:
+                entrance_x -= helper.total_x
+
+        for vehicle in state.vehicle_list:
+            if vehicle.loc != helper.goal_vehicle.loc and not vehicle.is_horizontal:
+                vehicle_tail = helper.get_vehicle_tail(vehicle)
+                if vehicle.loc[1] <= helper.goal_vehicle.loc[1] <= vehicle_tail[1]:
+                    if entrance_x > goal_vehicle_x:
+                        if goal_vehicle_x < vehicle.loc[0] < entrance_x:
+                            blocked_2 += 1
+                    else:
+                        if vehicle.loc[0] > goal_vehicle_x or vehicle.loc[0] < entrance_x:
+                            blocked_2 += 1
+    else:
+        goal_vehicle_y = helper.goal_vehicle_tail[1]
+        entrance_y = helper.goal_entrance[1]
+        # E
+        if not helper.is_entrance_inverted:
+            # goal_vehicle_x = helper.goal_vehicle_tail[0]
+            entrance_y += helper.goal_vehicle.length - 1
+            if entrance_y >= helper.total_y:
+                entrance_y -= helper.total_y
+
+        for vehicle in state.vehicle_list:
+            if vehicle.loc != helper.goal_vehicle.loc and vehicle.is_horizontal:
+                vehicle_tail = helper.get_vehicle_tail(vehicle)
+                if vehicle.loc[0] <= helper.goal_vehicle.loc[0] <= vehicle_tail[0]:
+                    if entrance_y > goal_vehicle_y:
+                        if goal_vehicle_y < vehicle.loc[0] < entrance_y:
+                            blocked_2 += 1
+                    else:
+                        if vehicle.loc[0] > goal_vehicle_y or vehicle.loc[0] < entrance_y:
+                            blocked_2 += 1
+
+    return min(moves_1 + blocked_1, moves_2 + blocked_2)
+
+
+def fval_function(sN, weight):
+    # IMPLEMENT
+    """
+  Provide a custom formula for f-value computation for Anytime Weighted A star.
+  Returns the fval of the state contained in the sNode.
+
+  @param sNode sN: A search node (containing a rush hour state)
+  @param float weight: Weight given by Anytime Weighted A star
+  @rtype: float
+  """
+
+    # Many searches will explore nodes (or states) that are ordered by their f-value.
+    # For UCS, the fvalue is the same as the gval of the state. For best-first search, the fvalue is the hval of the state.
+    # You can use this function to create an alternate f-value for states; this must be a function of the state and the weight.
+    # The function must return a numeric f-value.
+    # The value will determine your state's position on the Frontier list during a 'custom' search.
+    # You must initialize your search engine object as a 'custom' search engine if you supply a custom fval function.
+    return (weight * sN.hval) + sN.gval  # replace this
+
+
+def anytime_weighted_astar(initial_state, heur_fn, weight=1.0, timebound=10):
+    # IMPLEMENT
+    """Provides an implementation of anytime weighted a-star, as described in the HW1 handout"""
+    """INPUT: a rush hour state that represents the start state and a timebound (number of seconds)"""
+    """OUTPUT: A goal state (if a goal is found), else False"""
+    """implementation of weighted astar algorithm"""
+    time_remaining = timebound
+    se = SearchEngine('custom', 'full')
+    fval_wrap = (lambda sN: fval_function(sN, weight))
+    se.init_search(initial_state, rushhour_goal_fn, heur_fn, fval_wrap)
+    best_solution = float("inf")
+
+    start_time = os.times()[0]
+    result = se.search(time_remaining, costbound=(float("inf"), float("inf"), best_solution))
+    end_time = os.times()[0]
+
+    time_remaining = time_remaining - (end_time - start_time)
+
+    if result:
+        best_solution = result.gval + heur_fn(result)
+    else:
+        return False
+
+    while time_remaining > 0 and not se.open.empty():
+        start_time = os.times()[0]
+        better_result = se.search(time_remaining, (float("inf"), float("inf"), best_solution))
+        end_time = os.times()[0]
+        time_remaining = time_remaining - (end_time - start_time)
+        if better_result:
+            best_solution = better_result.gval + heur_fn(better_result)
+            result = better_result
+        else:
+            break
+
+    return result
+
+
+def anytime_gbfs(initial_state, heur_fn, timebound=10):
+    # IMPLEMENT
+    """Provides an implementation of anytime greedy best-first search, as described in the HW1 handout"""
+    """INPUT: a rush hour state that represents the start state and a timebound (number of seconds)"""
+    """OUTPUT: A goal state (if a goal is found), else False"""
+    """implementation of anytime greedybfs algorithm"""
+    search_engine = SearchEngine('best_first', 'full')
+    search_engine.init_search(initial_state, rushhour_goal_fn, heur_fn)
+
+    gval_cost_bound = float("inf")
+    time_left = timebound
+
+    init_time = os.times()[0]
+    solution = search_engine.search(timebound=time_left, costbound=(gval_cost_bound, float('inf'), float('inf')))
+    finish_time = os.times()[0]
+
+    time_left -= (finish_time - init_time)
+
+    if solution:
+        gval_cost_bound = solution.gval
+    else:
+        return False
+
+    while time_left > 0:
+        init_time = os.times()[0]
+        improved_solution = search_engine.search(timebound=time_left,
+                                                 costbound=(gval_cost_bound, float('inf'), float('inf')))
+        time_left -= (os.times()[0] - init_time)
+        if improved_solution:
+            gval_cost_bound = improved_solution.gval
+            solution = improved_solution
+        else:
+            break
+
+    return solution
